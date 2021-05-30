85ffc0ae15eb3214268c4300cbc601c3e1ba1a89
aro077a <aro077a@gmail.com>
2021-05-30 17:31:38 +0400

    fixed hover style and animation issue

--- src/components/works-container/items-wrapper/cardItems.tsx
+++ src/components/works-container/items-wrapper/cardItems.tsx
@@ -1,43 +1,42 @@
-import { FC, useEffect, useState } from "react";
-import "./cardItems.scss";
-import { CardImages } from "../fakeData";
-import Fade from "react-reveal/Fade";
+import { FC, useEffect, useState } from 'react';
+import { CardImages } from '../fakeData';
+import Fade from 'react-reveal/Fade';
+import './cardItems.scss';
 
 interface CardItemsProps {
-  sortLableName: string;
+  sortLabelName: string;
 }
 
-const CardItems: FC<CardItemsProps> = ({ sortLableName }) => {
+const CardItems: FC<CardItemsProps> = ({ sortLabelName }) => {
   const [isSorted, setIsSorted] = useState(false);
 
   useEffect(() => {
-    sortLableName === "all" ? setIsSorted(false) : setIsSorted(true);
-  }, [sortLableName]);
+    sortLabelName === 'all' ? setIsSorted(false) : setIsSorted(true);
+  }, [sortLabelName]);
 
-  
   return (
-    <Fade bottom big cascade duration={2500} distance="20000px">
-      <div className="cardItems__wrapper">
+    <div className='cardItems__wrapper'>
+      <Fade bottom big cascade duration={1500} delay={500} distance='30rem'>
         {CardImages &&
           CardImages.filter((item) =>
             !isSorted
-              ? item.type !== sortLableName
-              : item.type === sortLableName
+              ? item.type !== sortLabelName
+              : item.type === sortLabelName
           ).map((card, index) => (
-            <div key={index} className="cardItems__wrapper__card">
-              <div className="cardItems__wrapper__card--imageTitle">
+            <div key={index} className='cardItems__wrapper__card'>
+              <div className='cardItems__wrapper__card--imageTitle'>
                 <h1>{card.title}</h1>
               </div>
-              <div className="cardItems__wrapper__card--image">
-                <img src={card.image} alt="cardImage" />
+              <div className='cardItems__wrapper__card--image'>
+                <img src={card.image} alt='cardImage' />
               </div>
-              <div className="cardItems__wrapper__card--imageFooter">
+              <div className='cardItems__wrapper__card--imageFooter'>
                 <h4>{card.description}</h4>
               </div>
             </div>
           ))}
-      </div>
-    </Fade>
+      </Fade>
+    </div>
   );
 };
 
