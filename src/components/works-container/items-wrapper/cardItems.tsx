53b39fb03d7cc08619cbbb350ad740a605b20f6a
gevorgmanasyan2 <gevorgmanasyan2@gmail.com>
2021-06-01 13:01:47 +0400

    working on works page animation issues

--- src/components/works-container/items-wrapper/cardItems.tsx
+++ src/components/works-container/items-wrapper/cardItems.tsx
@@ -1,6 +1,5 @@
 import { FC, useEffect, useState } from 'react';
 import { CardImages } from '../fakeData';
-import Fade from 'react-reveal/Fade';
 import './cardItems.scss';
 
 interface CardItemsProps {
@@ -9,20 +8,35 @@ interface CardItemsProps {
 
 const CardItems: FC<CardItemsProps> = ({ sortLabelName }) => {
   const [isSorted, setIsSorted] = useState(false);
+  const[count,setCount]=useState(0); 
 
   useEffect(() => {
     sortLabelName === 'all' ? setIsSorted(false) : setIsSorted(true);
-  }, [sortLabelName]);
+   }, [sortLabelName]);
+
+  useEffect(()=>{
+   let counter=count; 
+   const interval=setInterval(()=>{
+     if(counter>=CardImages.length){
+       clearInterval(interval);
+     }    
+     else{
+       setCount(count=>count+1);
+       counter++;
+     }
+   },500);
+   
+   return()=>clearInterval(interval)  
+  },[count]);
 
   return (
-    <div className='cardItems__wrapper'>
-      <Fade bottom big cascade duration={1500} delay={500} distance='300rem'>
+    <div className='cardItems__wrapper'>     
         {CardImages &&
           CardImages.filter((item) =>
             !isSorted
               ? item.type !== sortLabelName
               : item.type === sortLabelName
-          ).map((card, index) => (
+          ).slice(0,count).map((card, index) => (
             <div key={index} className='cardItems__wrapper__card'>
               <div className='cardItems__wrapper__card--imageTitle'>
                 <h1>{card.title}</h1>
@@ -34,8 +48,7 @@ const CardItems: FC<CardItemsProps> = ({ sortLabelName }) => {
                 <h4>{card.description}</h4>
               </div>
             </div>
-          ))}
-      </Fade>
+          ))}      
     </div>
   );
 };
